9587ca512cbfcbb6452ec897997e734a3c12384d
Juliana <julianaakkemi@gmail.com>
2025-08-15 19:26:49 -0300

    add: creation of new task

--- src/components/TaskForm/index.tsx
+++ src/components/TaskForm/index.tsx
@@ -1,14 +1,67 @@
-import type { FunctionComponent } from "react";
+import { useRef, type FunctionComponent } from "react";
 import { DefaultInput } from "../DefaultInput";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../contexts/TaskContext/getNextCycle";
+import { getNextCycleType } from "../../contexts/TaskContext/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export const TaskForm: FunctionComponent = () => {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!taskNameInput.current) {
+      return;
+    }
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      duration: state.config[nextCycleType],
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
-    <form className="form" action="">
+    <form className="form" action="" onSubmit={handleCreateNewTask}>
       <div className="formRow">
-        <DefaultInput id="meuInput" type="text" label="Task" />
+        <DefaultInput
+          id="meuInput"
+          type="text"
+          label="Task"
+          ref={taskNameInput}
+        />
       </div>
 
       <div className="formRow">
